6295cb23df8460b8c7e6fe4c69e3e55146253fc5
davi1140 <66921140+davi1140@users.noreply.github.com>
2021-05-20 00:48:27 -0400

    Update prisoner.py

--- prisoner.py
+++ prisoner.py
@@ -2,33 +2,33 @@ import random
 import numpy as np
 
 # Analytical: First: I analyze you. I start:
-# Silent, Silent, Truth, Silent.
+# Silent, Truth, Silent.
 # If you "tell the truth", I'll act like [Tit for Tat] while still analyzing.
 # If you never "tell the truth", I'll act like [alwaysTellTruth] while still analyzing, because I'd rather try and get the best out of the situation. 
 # The world's a grim place, and I need to keep my head in it. Things can change on the fly whenever.
 
-# Basically, analyzing the entire situation rather than just one sample size should help increase point total.
+# Basically, analyzing the entire situation rather than just one sample size should help increase point total. (Better version of detective)
 # Reminder: For the history array, "tell truth" = 0, "stay silent" = 1
 
 def strategy(history, memory):
-    Schedule = ["stay silent","stay silent","tell truth","stay silent"]
+    Schedule = ["stay silent","tell truth","stay silent"]
     gLength = history.shape[1]
     Speak = memory
-    choice = [None] * (gLength / 4)
+    choice = None
     test = None
     Switches = 0
     counter = 0
     
     test = gLength
-    while(test > 1): # Testing whether the cycle has reached 4  or not
-        test = test % 4
+    while(test > 1): # Testing whether the cycle has reached a multiple of 3  or not
+        test = test % 3
         if(test > 0)
             Switches = 0 
         elif(test == 1):
             Switches = 1
             break
             
-    if (gLength < 4): # Testing range
+    if (gLength < 3): # Testing range
         choice = Schedule[gLength]
     elif (Switches == 1): # Time to analyze the testing stage and decide what to do based on what the opponent did in that time!
         opponentsActions = history[1]
@@ -40,8 +40,6 @@ def strategy(history, memory):
         if Speak:
             choice = "tell truth"
         else:
-            choice = "stay silent" 
-            if (history[1,-1] == 1) 
-            else "tell truth" # Do Tit for Tat
+            choice = "stay silent" if (history[1,-1] == 1) else "tell truth" # Do Tit for Tat
     
     return choice, Speak
